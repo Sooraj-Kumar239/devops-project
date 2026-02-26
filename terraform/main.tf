3adb712f2034356bf094289ad780f7eaca1c71f3
Sooraj-Kumar239 <skoad.2194@gmail.com>
2026-02-26 19:07:36 +0000

    Initial clean DevOps project structure

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "devop_sg" {
+  name        = "devop-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devop_server" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t3.micro"
+  key_name               = "devop-key"
+  vpc_security_group_ids = [aws_security_group.devop_sg.id]
+
+  tags = {
+    Name = "Devop-Project-Server"
+  }
+}
